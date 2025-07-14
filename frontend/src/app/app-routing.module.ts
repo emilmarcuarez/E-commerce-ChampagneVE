92001efcccf267e9f3ac55ab28ae63a0b8744b8c
emilmarcuarez <emilmarpatricia@gmail.com>
2025-07-13 20:08:50 -0400

    frontend del navbar listo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'shop', component:ShopComponent},
+  {path:'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'contact', component: ContactComponent},
+   {path:'**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
